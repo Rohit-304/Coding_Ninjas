095e8b9b701ad1c213e5336e5597fb95c1561f48
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-09-21 23:57:04 +0530

    Update Fahrenheit to Celsius.java

--- Basic_of_java/Fahrenheit to Celsius.java
+++ Basic_of_java/Fahrenheit to Celsius.java
@@ -1,6 +1,7 @@
 /*
 Problem Statement:Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E), and Step Size (W), you need to 
 convert all Fahrenheit values from Start to End at the gap of W into their corresponding Celsius values and print the table.
+
 Note:
 
 Print the floor of the Celsius values if they are non-negative else print its ceil value.
