09ee3284f55e12b77cec4bf25997829702cdf594
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-10-03 23:33:10 +0530

    Update Reverse String Word Wise.java

--- Basic_of_java/Reverse String Word Wise.java
+++ Basic_of_java/Reverse String Word Wise.java
@@ -5,7 +5,7 @@ theta <= |S| <= 10 ^ 5
 where |S| represents the length of string, S.
 Sample Input 1:
 Welcome to Coding Ninjas 
-Sample Output 1:
+Sample Output 1: 
 Ninjas Coding to Welcome
 Sample Input 2:
 Always indent your code
