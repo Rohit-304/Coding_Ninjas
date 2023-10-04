2747062d9f1cae8a4c49f892a9b3cf33abbc9b7f
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-10-04 22:54:15 +0530

    Update Reverse Each Word.java

--- Basic_of_java/Reverse Each Word.java
+++ Basic_of_java/Reverse Each Word.java
@@ -3,7 +3,7 @@ Problem Statement
 
 Aadil has been provided with a sentence in the form of a string as a function parameter. The task is to implement a function so as to print the sentence such that each word in the sentence is reversed.
 
-Example:
+Example: 
 
 Input Sentence: "Hello, I am Aadil!" The expected output will print, ",olleH I ma lli daA^ prime prime .
 
